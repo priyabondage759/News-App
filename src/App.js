9088b3e921433bf047eae730eb800d49be1d55d2
priyabondage759 <priyankabondage759@gmail.com>
2023-08-11 11:47:36 +0530

    News-App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import {NewsApp} from "./Pages/NewsApp"
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Register } from './Components/Register';
+import { Login } from './Components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/news' element={<NewsApp/>}/>
+      </Routes>
+      </BrowserRouter>
+     {/* <NewsApp/> */}
     </div>
   );
 }
